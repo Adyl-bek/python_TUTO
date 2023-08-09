53e78d5fab4f57139709647fa3239588674d6b04
Adyl-bek <adylbekmedina2005@gmail.com>
2023-08-09 18:07:44 +0600

    Finish my quiz witn only 3 questions

--- quiz.py
+++ quiz.py
@@ -20,12 +20,15 @@ class Question():
         self.correct = corr_answer
         self.answers = all_answers
     def result(self) -> None:
+        """Adds 1 point to player's score if he answers correctly"""
         answer = input("Your answer is: ")
         if(answer == self.correct):
+            print("")
             print("It's correct")
             User["Score"] += 1
         else:
-            print("Correct answer was on number " + self.correct + " not on number", answer)
+            print("")
+            print("Correct answer was on number " + self.correct + ", not on number", answer)
 
     # def new_method(self):
     #     return 1
@@ -33,15 +36,37 @@ class Question():
 
 
         
-question_1 = Question("1.", "How many months of the year has 28 days?", "2", "1) 12\n2) 1\n3) 11\n4) None")
-question_2 = Question("2.", "57+23=0\n10+12=1\n85+69=4\n78+89=?", "2", "1) 0\n2) 167\n3) 5\n4) 3")
+question_1 = Question("1.", "How many months of the year has 28 days?", "1", "1) 12\n2) 1\n3) 11\n4) None")
+question_2 = Question("2.", "57+23=0\n10+12=1\n85+69=4\n78+89=?", "3", "1) 0\n2) 167\n3) 5\n4) 3")
+question_3 = Question("3.", "Which planet in our solar system is known as the 'Red Planet'?", "4", "1) Mercury\n2) Saturn\n3) Sun\n4) Mars")
 
 
 print(question_1.que_number, "" + question_1.que_text)
 print(question_1.answers)
 question_1.result()
 print(User["Name"] + " your current score is", User["Score"])
+print("--------------------")
 
+print(question_2.que_number, "" + question_2.que_text)
+print(question_2.answers)
+question_2.result()
+print(User["Name"] + " your current score is", User["Score"])
+print("--------------------")
+
+print(question_3.que_number, "" + question_3.que_text)
+print(question_3.answers)
+question_3.result()
+print(User["Name"] + " your current score is", User["Score"])
+print("--------------------")
+
+if(User["Score"] == 3):
+    print("CONGRATS. YOU ARE THE SMARTEST")
+elif(User["Score"] == 2):
+    print("Not Bad")
+elif(User["Score"] == 1):
+    print("You can better")
+else:
+    print("It's pretty bad, maybe try again(")
 
 
 # print(question_1.que_number, "" + question_1.que_text)
