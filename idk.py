1773e43edca3879e99a7ec423dc59f5acdd4924e
Adyl-bek <adylbekmedina2005@gmail.com>
2023-08-24 14:30:29 +0600

    Just checking Er's code

--- idk.py
+++ idk.py
@@ -1,23 +1,29 @@
-from math import *
-
+# from math import *
 # print("Adylbek \nis \na \nprogrammer")
-person_name = "Adylbek Rakhmanov"
-person_age = 18
+# person_name = "Adylbek Rakhmanov"
+# person_age = 18
 # person_is_male = True
 # print(person_name , "came to NAU couple of days ago.")
 # print("The new student is" , person_age)
 # print(person_name.upper().islower())
 # print(person_name.replace("Adylbek", "Adyl"))
-num = -6
+# num = -6
 # print(abs(num))
 # print(min(15, 67))
 # print(floor(4.6))
 # print(ceil(4.6))
 # print(sqrt(16))
+# print(dir("."))
 
 
+# Import the 'string' module
+# import string
 
+# # Create a string containing whitespace, punctuation, digits, and letters
+# chars = " " + string.punctuation + string.digits + string.ascii_letters
 
+# # Convert the characters string into a list of individual characters
+# chars = list(chars)
 
-
-print(dir("."))
+# # Print the list of characters
+# print(chars)
