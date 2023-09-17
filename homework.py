c62ebc7a0af1c681f6b310b0ecf7018a407e4616
Adyl-bek <adylbekmedina2005@gmail.com>
2023-09-17 21:37:34 +0600

    Done hw

--- homework.py
+++ homework.py
@@ -1,62 +1,21 @@
-from math import sqrt
-
-def interface():
-    print("\nWelcome to the Interactive Calculator:")
-    print("1. Addiction\n2. Substarction\n3. Multiplication\n4. Division\n5. Square Root\n6. Quit")
-
-def add(a, b):
-    print(f"Result: {first_num} + {second_num} = {a + b}")
-
-def sub(a, b):
-    print(f"Result: {first_num} - {second_num} = {a - b}")
-
-def mul(a, b):
-    print(f"Result: {first_num} * {second_num} = {a * b}")
-
-def div(a, b):
-    if(b != 0):
-        print(f"Result: {first_num} / {second_num} = {a / b}")
+score = int(input("Enter your score: "))
+A_SCORE = 90
+B_SCORE = 80
+C_SCORE = 70
+D_SCORE = 60
+while(score >= 0 and score <= 100):
+    if score >= A_SCORE:
+        print('Your grade is A.')
+        break
+    elif score >= B_SCORE:
+        print('Your grade is B.')
+        break
+    elif score >= C_SCORE:
+        print('Your grade is C.')
+        break
+    elif score >= D_SCORE:
+        print('Your grade is D.')
+        break
     else:
-        print("Error")
-
-def sqr():
-    chs = int(input("Enter a number: "))
-    if(chs >= 0):
-        print(f"Result: Square root of {chs} = {sqrt(chs)}")
-    else:
-        print("Error")
-    
-def ext():
-    print("Goodbye!")
-    quit()
-    
-# interface()        
-# choice = int(input("Enter your choice (1/2/3/4/5/6): "))
-# choice = 1
-
-while(1):  
-    interface()
-    choice = float(input("Enter your choice (1/2/3/4/5/6): "))
-    if(choice != 5 and choice != 6):
-        first_num = float(input("Enter the first number: "))
-        second_num = float(input("Enter the second number: "))
-        if(choice == 1):
-            add(first_num, second_num)
-        elif(choice == 2):
-            sub(first_num, second_num)
-        elif(choice == 3):
-            mul(first_num, second_num)
-        elif(choice == 4):
-            div(first_num, second_num)
-        # interface()
-        # choice = int(input("Enter your choice (1/2/3/4/5/6): "))
-        # first_num = int(input("Enter the first number: "))
-        # second_num = int(input("Enter the second number: "))
-    elif(choice == 5):
-            sqr()
-            interface()
-            choice = float(input("Enter your choice (1/2/3/4/5/6): "))
-            first_num = float(input("Enter the first number: "))
-            second_num = float(input("Enter the second number: "))
-    else:
-        ext()
+        print('Your grade is F.')
+        break
