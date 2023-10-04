689007870417c99a0a26bfcb08c2777769724098
Adyl-bek <adylbekmedina2005@gmail.com>
2023-10-04 21:52:01 +0600

    second day of using 100 days python course

---
+++ 100daysofcoding.py
@@ -0,0 +1,10 @@
+"""Dau_2 / 10.04.2023"""
+# num = int(input())
+# res = int((num/10)) + (num%10)
+# print(res)
+
+height = input()
+weight = input()
+
+bmi = int(weight) / float(height) ** 2  
+print(int(bmi)) 
--- homework.py
+++ homework.py
@@ -1,21 +1,34 @@
-score = int(input("Enter your score: "))
-A_SCORE = 90
-B_SCORE = 80
-C_SCORE = 70
-D_SCORE = 60
-while(score >= 0 and score <= 100):
-    if score >= A_SCORE:
-        print('Your grade is A.')
-        break
-    elif score >= B_SCORE:
-        print('Your grade is B.')
-        break
-    elif score >= C_SCORE:
-        print('Your grade is C.')
-        break
-    elif score >= D_SCORE:
-        print('Your grade is D.')
-        break
+# score = int(input("Enter your score: "))
+# A_SCORE = 90
+# B_SCORE = 80
+# C_SCORE = 70
+# D_SCORE = 60
+# while(score >= 0 and score <= 100):
+#     if score >= A_SCORE:
+#         print('Your grade is A.')
+#         break
+#     elif score >= B_SCORE:
+#         print('Your grade is B.')
+#         break
+#     elif score >= C_SCORE:
+#         print('Your grade is C.')
+#         break
+#     elif score >= D_SCORE:
+#         print('Your grade is D.')
+#         break
+#     else:
+#         print('Your grade is F.')
+#         break
+
+
+a = 0
+
+for i in range(10):
+
+    if i%2 == 0:
+
+        a += i/2
+
     else:
-        print('Your grade is F.')
-        break
+
+        a += i
